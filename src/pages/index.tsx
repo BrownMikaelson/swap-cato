22dcbed3a913bf0fa9a165e0a13c0b7fcb1043ce
Brown Mikaelson <50189679+BrownMikaelson@users.noreply.github.com>
2024-08-21 14:42:19 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export async function getStaticProps() {
 
 export default function Aggregator(props) {
 	return (
-		<Layout title={`Mercury Swap Aggregator`} defaultSEO>
+		<Layout title={`Pessi Swap Aggregator`} defaultSEO>
 			<ConnectButton />
 			<AggregatorContainer tokenlist={props.tokenlist} />
 		</Layout>
