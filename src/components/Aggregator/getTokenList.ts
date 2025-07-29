b87626284e20aabb383ccdeaee2620b553ad3795
Brown Mikaelson <50189679+BrownMikaelson@users.noreply.github.com>
2025-07-28 23:24:30 -0300

    Update getTokenList.ts

--- src/components/Aggregator/getTokenList.ts
+++ src/components/Aggregator/getTokenList.ts
@@ -1,83 +1,106 @@
-import { groupBy, mapValues, merge, uniqBy } from 'lodash';
 import { IToken } from '~/types';
 import { nativeTokens } from './nativeTokens';
+import { groupBy, merge, mapValues, uniqBy } from 'lodash';
 
 const tokensToRemove = {
-	1: {
-		['0xB8c77482e45F1F44dE1745F52C74426C631bDD52'.toLowerCase()]: true
-	}
+  1: {
+    ['0xB8c77482e45F1F44dE1745F52C74426C631bDD52'.toLowerCase()]: true
+  }
 };
 
 const oneInchChains = {
-	ethereum: 1,
-	bsc: 56,
-	polygon: 137,
-	optimism: 10,
-	arbitrum: 42161,
-	avax: 43114,
-	gnosis: 100,
-	fantom: 250,
-	klaytn: 8217
+  ethereum: 1,
+  bsc: 56,
+  polygon: 137,
+  optimism: 10,
+  arbitrum: 42161,
+  avax: 43114,
+  gnosis: 100,
+  fantom: 250,
+  klaytn: 8217
 };
 
 export async function getTokenList() {
-	// const uniList = await fetch('https://tokens.uniswap.org/').then((r) => r.json());
-	// const sushiList = await fetch('https://token-list.sushi.com/').then((r) => r.json());
-	const oneInch = await Promise.all(
-		Object.values(oneInchChains).map(async (chainId) =>
-			fetch(`https://tokens.1inch.io/v1.1/${chainId}`).then((r) => r.json())
-		)
-	);
-	// const hecoList = await fetch('https://token-list.sushi.com/').then((r) => r.json()); // same as sushi
-	// const lifiList = await fetch('https://li.quest/v1/tokens').then((r) => r.json());
+  let uniList = { tokens: [] }, sushiList = { tokens: [] }, lifiList = { tokens: [] }, geckoList = [];
 
-	const [uniList, sushiList, lifiList, geckoList] = await Promise.all([
-		fetch('https://tokens.uniswap.org/').then((r) => r.json()),
-		fetch('https://token-list.sushi.com/').then((r) => r.json()),
-		fetch('https://li.quest/v1/tokens').then((r) => r.json()),
-		fetch('https://api.coingecko.com/api/v3/coins/list?include_platform=false').then((res) => res.json())
-	]);
+  try {
+    [uniList, sushiList, lifiList, geckoList] = await Promise.all([
+      fetch('https://tokens.uniswap.org/').then((r) => r.json()).catch(() => ({ tokens: [] })),
+      fetch('https://token-list.sushi.com/').then((r) => r.json()).catch(() => ({ tokens: [] })),
+      fetch('https://li.quest/v1/tokens').then((r) => r.json()).catch(() => ({ tokens: [] })),
+      fetch('https://api.coingecko.com/api/v3/coins/list?include_platform=false').then((res) => res.json()).catch(() => [])
+    ]);
+  } catch (e) {
+    uniList = { tokens: [] }; 
+    sushiList = { tokens: [] }; 
+    lifiList = { tokens: [] }; 
+    geckoList = [];
+  }
 
-	const oneInchList = Object.values(oneInchChains)
-		.map((chainId, i) =>
-			Object.values(oneInch[i]).map((token: { address: string }) => ({
-				...token,
-				chainId
-			}))
-		)
-		.flat();
+  let oneInch = [];
+  try {
+    oneInch = await Promise.all(
+      Object.values(oneInchChains).map(async (chainId) =>
+        fetch(`https://tokens.1inch.io/v1.1/${chainId}`)
+          .then((r) => r.json())
+          .catch(() => ({}))
+      )
+    );
+  } catch (e) {
+    oneInch = [];
+  }
 
-	const tokensByChain = mapValues(
-		merge(
-			groupBy([...oneInchList, ...sushiList.tokens, ...uniList.tokens, ...nativeTokens], 'chainId'),
-			lifiList.tokens
-		),
-		(val) => uniqBy(val, (token: IToken) => token.address.toLowerCase())
-	);
+  const oneInchList = Object.values(oneInchChains)
+    .map((chainId, i) =>
+      Object.values(oneInch[i] || {}).map((token: { address: string }) => ({
+        ...token,
+        chainId
+      }))
+    )
+    .flat();
 
-	const tokensFiltered = mapValues(tokensByChain, (val, key) => {
-		return val.filter((token) => !tokensToRemove[key]?.[token.address.toLowerCase()]);
-	});
+  const tokensByChain = mapValues(
+    merge(
+      groupBy(
+        [
+          ...oneInchList,
+          ...(sushiList.tokens || []),
+          ...(uniList.tokens || []),
+          ...nativeTokens
+        ],
+        'chainId'
+      ),
+      lifiList.tokens || {}
+    ),
+    (val) => uniqBy(val, (token: IToken) => token.address.toLowerCase())
+  );
 
-	const tokenlist = {};
+  const tokensFiltered = mapValues(tokensByChain, (val, key) => {
+    return val.filter((token) => !tokensToRemove[key]?.[token.address.toLowerCase()]);
+  });
 
-	for (const chain in tokensFiltered) {
-		tokenlist[chain] = tokensFiltered[chain].map((t) => ({
-			...t,
-			label: t.symbol,
-			value: t.address,
-			geckoId: geckoList
-				? geckoList?.find((geckoCoin) => geckoCoin.symbol === t.symbol.toLowerCase())?.id ?? null
-				: null
-		}));
-	}
+  const tokenlist: Record<string, any[]> = {};
 
-	tokenlist[66][0].logoURI = tokenlist[66][1].logoURI;
+  for (const chain in tokensFiltered) {
+    tokenlist[chain] = tokensFiltered[chain].map((t) => ({
+      ...t,
+      label: t.symbol,
+      value: t.address,
+      geckoId: geckoList
+        ? geckoList?.find((geckoCoin) => geckoCoin.symbol === t.symbol.toLowerCase())?.id ?? null
+        : null
+    }));
+  }
 
-	return {
-		props: {
-			tokenlist
-		},
-		revalidate: 5 * 60 // 5 minutes
-	};
+  // Evita erro se tokenlist[66] não existe ou está vazio
+  if (tokenlist[66] && tokenlist[66][0] && tokenlist[66][1]) {
+    tokenlist[66][0].logoURI = tokenlist[66][1].logoURI;
+  }
+
+  return {
+    props: {
+      tokenlist
+    },
+    revalidate: 5 * 60 // 5 minutos
+  };
 }
